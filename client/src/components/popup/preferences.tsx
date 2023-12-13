0687c4a06040b8c6d166a7feb71fcf6f7f575ca7
ran <redamancy0326@gmail.com>
2023-12-13 20:18:43 +0800

    Add a basic slider to the UI to switch mobile/auto/desktop

--- client/src/components/popup/preferences.tsx
+++ client/src/components/popup/preferences.tsx
@@ -1,51 +1,49 @@
 import * as React from 'react'
-import { Input, Typography } from '@mui/material'
-import Markdown from '../markdown'
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
+import { FormControlLabel, Switch, Typography, Slider } from '@mui/material';
 
 import { IMobileContext } from "../infoview/context"
 
-interface PreferencesPopupProps extends IMobileContext{
+import "../../css/preferences.css"
+
+interface PreferencesPopupProps extends IMobileContext {
     handleClose: () => void
-} 
+}
+
+const marks = [
+    {
+        value: 0,
+        label: 'Mobile',
+    },
+    {
+        value: 1,
+        label: 'Auto',
+    },
+    {
+        value: 2,
+        label: 'Desktop',
+    },
+];
+
 
 export function PreferencesPopup({ mobile, setMobile, lockMobile, setLockMobile, handleClose }: PreferencesPopupProps) {
     return <div className="modal-wrapper">
         <div className="modal-backdrop" onClick={handleClose} />
         <div className="modal">
             <div className="codicon codicon-close modal-close" onClick={handleClose}></div>
-            <Typography variant="body1" component="div" className="settings">
+            <Typography variant="body1" component="div" className="references">
                 <div className='preferences-category'>
                     <div className='category-title'>
-                        <h3>Mobile layout</h3>
+                        <h3>Layout</h3>
                     </div>
-                    <div className='preferences-item'>
-                        <FormControlLabel
-                            control={
-                            <Switch
-                                checked={mobile}
-                                onChange={() => setMobile(!mobile)}
-                                name="checked"
-                                color="primary"
-                            />
-                            }
-                            label="Enable"
-                            labelPlacement="start"
-                        />
-                    </div>
-                    <div className='preferences-item'>
-                        <FormControlLabel
-                            control={
-                            <Switch
-                                checked={!lockMobile}
-                                onChange={() => setLockMobile(!lockMobile)}
-                                name="checked"
-                                color="primary"
-                            />
-                            }
-                            label="Auto"
-                            labelPlacement="start"
+                    <div className='preferences-item narrow'>
+                        <Slider
+                            aria-label="Restricted values"
+                            defaultValue={1}
+                            max={2}
+                            // getAriaValueText={valuetext}
+                            step={1}
+                            valueLabelDisplay="off"
+                            marks={marks}
                         />
                     </div>
                 </div>
